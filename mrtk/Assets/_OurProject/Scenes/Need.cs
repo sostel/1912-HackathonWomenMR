b11295b124794b8a0b677c770281d1ce2ec00873
Bernadette Thalhammer <bethalha@microsoft.com>
2019-12-07 17:59:00 -0800

    added pet and need class with debug display and needs increasing over time

---
+++ mrtk/Assets/_OurProject/Scenes/Need.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Need : MonoBehaviour
+{
+
+    public float value = 0;
+    public float incrementPerHour = 10;
+    public string needName = "Need";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTimeInSeconds = Time.deltaTime;
+        float deltaTimeInHours = deltaTimeInSeconds / 3600;
+
+        float currentFrameIncrement = incrementPerHour * deltaTimeInHours;
+
+        value += currentFrameIncrement;
+    }
+}
---
+++ mrtk/Assets/_OurProject/Scenes/Pet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+
+
+    public List<Need> needs;
+
+    // debug
+    public TextMeshPro debugLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (debugLabel != null)
+        {
+            string labelText = "Stats \n";
+            for (int i = 0; i < needs.Count; ++i)
+            {
+                labelText += "\n";
+                labelText += needs[i].needName;
+                labelText += "; ";
+                labelText += needs[i].value;
+            }
+
+            debugLabel.SetText(labelText);
+        }
+    }
+}
