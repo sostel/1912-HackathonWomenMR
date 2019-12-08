0061158b9cc2e5f0d30c6512680d7e22faaf26d0
Sophie Stellmach <sostel@microsoft.com>
2019-12-07 18:22:42 -0800

    Adding custom follow behavior
    It's a start...

---
+++ mrtk/Assets/_OurProject/Scripts/FollowBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+public class FollowBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float minDistanceInMeters = 0.6f;
+
+    [SerializeField]
+    private float moveSpeed = 0.01f;
+
+    [SerializeField]
+    private bool isFollowing = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(CameraCache.Main.transform.position); // TODO: Implement more natural behavior
+
+        if (isFollowing)
+        {
+            Vector3 newPos = this.transform.position + (CameraCache.Main.transform.position - this.transform.position).normalized * moveSpeed;
+            
+            if (Vector3.Distance(CameraCache.Main.transform.position, newPos) > minDistanceInMeters)
+            {
+                this.transform.position = newPos;
+            }
+        }
+    }
+}
